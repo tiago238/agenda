bf134ce0ce1072f688f6e5d2ea793b0d277f3b08
Tiago Martins <tiago_martins238@hotmail.com>
2023-08-06 08:44:26 -0300

    created list os contacts into content page

--- contact/urls.py
+++ contact/urls.py
@@ -4,6 +4,7 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('<int:contact_id>/', views.contact, name='contact'), # type: ignore
+    path('', views.index, name='index'), # type: ignore
 ]
 
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 
@@ -13,4 +13,17 @@ def index(request):
         request, 
         'contact/index.html',
         context=context
+    )
+
+
+def contact(request, contact_id):
+    #single_contact = Contact.objects.get(id=contact_id)
+    single_contact = get_object_or_404(Contact, id=contact_id, show=True)
+    context = {
+        'contact': single_contact,
+    }
+    return render(
+        request,
+        'contact/contact.html',
+        context       
     )
