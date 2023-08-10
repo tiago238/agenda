55abcf4e966a951b19b99486ad6affc790ffb113
Tiago Martins <tiago_martins238@hotmail.com>
2023-08-09 22:56:39 -0300

    create pagination

--- contact/urls.py
+++ contact/urls.py
@@ -5,6 +5,7 @@ app_name = 'contact'
 
 urlpatterns = [
     path('<int:contact_id>/', views.contact, name='contact'), # type: ignore
+    path('search/', views.search, name='search'), # type: ignore
     path('', views.index, name='index'), # type: ignore
 ]
 
