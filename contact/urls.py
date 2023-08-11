52d7a53793521f9df0ff52d53c225eb291ae225b
Tiago Martins <tiago_martins238@hotmail.com>
2023-08-10 23:28:39 -0300

    Created page html create and view contact_forms

--- contact/urls.py
+++ contact/urls.py
@@ -1,11 +1,17 @@
+# type: ignore
 from django.urls import path
 from contact import views
 
 app_name = 'contact'
 
-urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'), # type: ignore
-    path('search/', views.search, name='search'), # type: ignore
-    path('', views.index, name='index'), # type: ignore
+urlpatterns = [     
+    path('search/', views.search, name='search'), 
+    path('', views.index, name='index'), 
+
+    # contact (CRUD)
+    path('contact/<int:contact_id>/detail/', views.contact, name='detail'),
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.contact, name='update'),
+    path('contact/<int:contact_id>/delete/', views.contact, name='delete'),
 ]
 
